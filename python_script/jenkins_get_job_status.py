ceb1bb68a628d2539f051f9ca1d6814433991219
CarlRazmus <carl.razmus@outlook.com>
2017-04-07 14:28:36 +0200

    removed unused code

--- python_script/jenkins_get_job_status.py
+++ python_script/jenkins_get_job_status.py
@@ -5,7 +5,6 @@ jenkins = Jenkins('http://ci.attentec.se')
 
 jobs = jenkins.get_job_info("Hacka-Traffic-Light", 0, False)
 last_completed_job = jobs["lastCompletedBuild"]
-#build = jobs["_class"]
 build_number = last_completed_job["number"]
 build_info = jenkins.get_build_info("Hacka-Traffic-Light", build_number) 
 result = build_info["result"]
