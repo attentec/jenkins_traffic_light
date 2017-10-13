3faf38f0df6a38f87baaead1bce19bb205801d69
Fredrik Rosenqvist <rosenqvistfredrik@hotmail.com>
2017-10-13 15:43:02 +0200

    Implemented cool animation

--- python_script/progress_bar.py
+++ python_script/progress_bar.py
@@ -3,9 +3,9 @@ import random
 import time
 from threading import Thread
 from queue import Queue, Empty
-
+import sys
 q = Queue()
-
+t = None
 def genRandomPayload(payload):
 	for led in range(30):
 		payload["led" + str(led)] = hex(random.randint(0,255))[2:].upper() + hex(random.randint(0,127))[2:].upper() + hex(random.randint(0,63))[2:].upper()
@@ -17,11 +17,13 @@ def updateLeds(payload):
 		pass
 
 def startUpThread():
+	global t
 	t = Thread(target=worker, args=(q,))
 	t.start()
 
 def killThread():
-	q.put(("kill"))
+	q.put("kill")
+	t.join()
 
 def turnOfAllLeds():
 	payload = {"quiet": "1"}
@@ -53,29 +55,26 @@ def setLedsUnstable():
 def worker(q):
 	ledsLighted = 0
 	p = {"quiet": "1"}
-	blinkingLedOn = True
+	blankLed = 0
 	while True:
 		try:
 			item = q.get(timeout=0.5)
 			if item[0] == "percent":
 				ledsLighted = int(30 * (int(item[1]) / 100))
-			elif item[1] == "kill":
-				return
+			elif item == "kill":
+				sys.exit()
 		except Empty as e:
 			pass
 
-		blinkingLed = ledsLighted + 1
+
+
 		for led in range(30):
 			if led <= ledsLighted:
 				p["led" + str(led)] = "0000FF"
 			else:
 				p["led" + str(led)] = "000000"
 
-		if blinkingLedOn:
-			p["led" + str(blinkingLed)] = "000000"
-			blinkingLedOn = False
-		else:
-			p["led" + str(blinkingLed)] = "0000FF"
-			blinkingLedOn = True
+		blankLed = (blankLed+1)%ledsLighted
+		p["led"+str(blankLed)] = "000000"
 
 		updateLeds(p)
